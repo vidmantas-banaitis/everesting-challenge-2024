343e5b427109b78b92ce7ed48baaa1f5c76c2da4
Vidmantas Banaitis <vidmantas.banaitis@gmail.com>
2024-05-19 00:03:27 +0300

    solution

---
+++ everesting.py
@@ -0,0 +1,52 @@
+import math
+
+# Description: Everesting challenge
+f = 'duomenys.txt'
+everest = 8848  # Everestas
+
+A = 0  # Vieno rato bendras vertikalus pakilimas
+B = 0  # Ratų skaičius įveikti dvigubą Everestą
+Cl = 0  # viso rato ilgis
+Ck = 0  # kilimo atkarpų ilgis
+Dn = ''  # stačiausios atkarpos pavadinimas
+Dk = 0  # stačiausios atkarpos pakilimo kampas laipsniais
+
+first = None
+it = first
+
+with open(f, 'r') as file:
+    for line in file:
+        row = line.split()
+        print(row)
+        n = {'data': {'name': row[0],
+                      'elevation': int(row[1]),
+                      'length': int(row[2])
+                      }, 'next': first}
+        if first is None:
+            first = n
+            it = first
+        else:
+            it['next'] = n
+            it = n
+
+it = first
+while True:
+    Cl += it['data']['length']
+    it['data']['ascending'] = it['next']['data']['elevation'] - it['data']['elevation']
+    it['data']['angle'] = math.degrees(math.atan(it['data']['ascending'] / it['data']['length']))
+    if it['data']['ascending'] > 0:
+        A += it['data']['ascending']
+        Ck += it['data']['length']
+    if it['data']['angle'] > Dk:
+        Dk = it['data']['angle']
+        Dn = it['data']['name']
+    print(it['data'])
+    if it['next'] is first:
+        break
+    it = it['next']
+
+B = (2*everest) / Ck
+print(f"A: {A}")
+print(f"B: {B}")
+print(f"C: {Cl}, {Ck}")
+print(f"D: {Dn}, {Dk}")
---
+++ readme.md
@@ -0,0 +1,29 @@
+## Everestingas
+
+Everestingas yra bėgimas, kurio metu siekiama surinkti tiek vertikalių metrų pakilimo, kad bėgantysis virtualiai užbėgtų
+ant aukščiausios pasaulio viršukalnės Everesto – t. y. pakilti į 8848 m aukštį. Bėgimo trasą sudaro pakilimai ir
+nusileidimai, kai grįžtama į tą pačią vietą, t.y. susidaro uždaras ratas. 
+
+### Iššūkis
+https://open.ktu.edu/mod/scorm/view.php?id=17402
+
+### Užduotis
+
+Trasos analizei faile pateikiamas nuosekliai
+einančių atkarpų sąrašas, kur kiekvienoje eilutėje nurodomas atkarpos simbolinis kodas, atkarpos pradinis aukštis ir
+atkarpos ilgis. Paskutinės atkarpos baigiamasis aukštis sutampa su pradinės atkarpos pradiniu aukščiu. Reikia rasti ir
+atspausdinti tokius parametrus (kartu su klausimo raide):
+
+A: koks yra vieno rato bendras vertikalus pakilimas?
+
+B: kiek ratų reikia užbaigti, kad būtų įveiktas dvigubas Everestas?
+
+C: koks viso rato ilgis ir koks bendras kilimo atkarpų ilgis?
+
+D: koks stačiausios pakilimo atkarpos kodas ir pakilimo kampas laipsniais?
+
+#### Plačiau apie everestingą:
+
+https://en.wikipedia.org/wiki/Everesting
+https://everesting.cc/
+https://www.lrt.lt/naujienos/sportas/10/2267246/gariunu-kalvos-everesta-vertes-begikas-dekingas-ultrabendruomenei-didziausias-dziaugsmas
